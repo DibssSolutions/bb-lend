7898ce048099f87621c9fd14fbd02f33620f7b67
Ilya khvesevich <rairetdev@gmail.com>
2018-10-19 16:57:31 +0300

    testimonials block: uncompleted desktop version

---
+++ src/js/components/_slider.js
@@ -0,0 +1,25 @@
+import slick from 'slick-carousel';
+import {eq} from 'semver';
+// $('.slider-for').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   fade: true,
+//   asNavFor: '.slider-nav'
+// });
+$('.testimonials__list').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  dots: true
+}).on({
+  beforeChange: function(event, slick, currentSlide, nextSlide) {
+    $('.testimonials__item').eq(nextSlide).toggleClass('testimonials__item_active');
+    $('.testimonials__item').eq(currentSlide).toggleClass('testimonials__item_active');
+  }
+});
+//
+// $('a[data-slide]').click(function(e) {
+//   e.preventDefault();
+//   var slideno = $(this).data('slide');
+//   $('.slider-nav').slick('slickGoTo', slideno - 1);
+// });
